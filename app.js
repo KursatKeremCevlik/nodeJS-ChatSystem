097e8491ba3f840a9188c6a6cfbe663d293e2772
KursatKeremCevlik <kur.ker.cev@gmail.com>
2020-08-20 23:27:16 +0300

    chatScreenRoutes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const LogIn = require('./routes/login');
 const SıgnIn = require('./routes/SignIn');
+const Chat = require('./routes/chat');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/LogIn', LogIn);
 app.use('/SignIn', SıgnIn);
+app.use('/Chat', Chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
